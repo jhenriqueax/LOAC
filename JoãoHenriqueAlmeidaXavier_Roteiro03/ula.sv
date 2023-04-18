d41526f2598cd4621f1ba08be492a6df691b72ca
João Henrique Almeida Xavier <63925427+jhenriqueax@users.noreply.github.com>
2023-04-18 15:10:46 -0300

    R3

--- JoãoHenriqueAlmeidaXavier_Roteiro03/ula.sv
+++ JoãoHenriqueAlmeidaXavier_Roteiro03/ula.sv
@@ -1,3 +1,4 @@
+// João Henrique Almeida Xavier - 121110402
 module circuit(input logic [7:0] A, B,
 				input logic [1:0] vetor, 
                	output logic [7:0] saida, 
@@ -11,12 +12,12 @@ module circuit(input logic [7:0] A, B,
           	'b01: saida <= A | B;
 			'b10: begin
               saida <= A + B;
-                if (saida < A & saida < B) // detecta overflow
+                if ((A[7] == B[7]) && (saida[7] != A[7])) // detecta overflow
                   FLAG_O <= 1;
                   end
               'b11: begin
                   saida <= A - B;
-                if (saida > A & saida > B) // detecta underflow
+                if ((A[7] != B[7]) && (saida[7] != A[7])) // detecta underflow
                   FLAG_O <= 1;
                   end endcase
               end
